211bf0b276517803c006df8ac44669756c40db83
Alain Schlesser <alain.schlesser@gmail.com>
2021-03-16 18:44:21 +0000

    Re-add attribute constant that was lost on rebase

--- src/Attribute.php
+++ src/Attribute.php
@@ -1003,6 +1003,7 @@ interface Attribute
     const DATA_GFYID                       = 'data-gfyid';
     const DATA_GISTID                      = 'data-gistid';
     const DATA_HERO                        = 'data-hero';
+    const DATA_HERO_CANDIDATE              = 'data-hero-candidate';
     const DATA_HREF                        = 'data-href';
     const DATA_ID                          = 'data-id';
     const DATA_IFRAME_SRC                  = 'data-iframe-src';
